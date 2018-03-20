a33907bee19c10605183d99eda9c62298bf09a13
Kaio Henrique <kaiohenriquedesousacosta3@gmail.com>
2018-03-20 14:09:10 -0300

    - Criação de métodos para intent da Camera e Galeria

--- app/src/main/java/br/com/test/kaioh/exifcamera/MainActivity.java
+++ app/src/main/java/br/com/test/kaioh/exifcamera/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.test.kaioh.exifcamera;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.database.Cursor;
@@ -9,24 +10,33 @@ import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
 import android.os.Parcelable;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private TextView pathImage;
+    private Button btnCamera, btnGallery;
+    private String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +45,23 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.imageView);
         pathImage = findViewById(R.id.pathImage);
+        btnCamera = findViewById(R.id.btnCamera);
+        btnGallery = findViewById(R.id.btnGallery);
 
         final PermissionManager permissionManager = new PermissionManager();
 
-        imageView.setOnClickListener(new View.OnClickListener() {
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (permissionManager.userHasPermission(MainActivity.this)){
+                    takePicture();
+                } else {
+                    permissionManager.requestPermission(MainActivity.this);
+                }
+            }
+        });
+
+        btnGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (permissionManager.userHasPermission(MainActivity.this)){
@@ -49,6 +72,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            Uri photoURI = null;
+            try {
+                File photoFile = createImageFileWith();
+                path = photoFile.getAbsolutePath();
+                photoURI = FileProvider.getUriForFile(this,
+                        getString(R.string.file_provider_authority),
+                        photoFile);
+
+            } catch (IOException ex) {
+                Log.e("TakePicture", ex.getMessage());
+            }
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP) {
+                takePictureIntent.setClipData(ClipData.newRawUri("", photoURI));
+                takePictureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            }
+            startActivityForResult(takePictureIntent, 1);
+        }
+    }
+    public File createImageFileWith() throws IOException {
+        Locale locale = new Locale("pt", "BR");
+        final String timestamp = new SimpleDateFormat("yyyyMMddHHmmss", locale).format(new Date());
+        final String imageFileName = "Atestado_" + timestamp;
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "EXIF");
+        if (!storageDir.exists()) {
+            storageDir.mkdirs();
+        }
+        return File.createTempFile(imageFileName, ".jpg", storageDir);
     }
 
     private void pickFromGallery(){
@@ -85,30 +141,176 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK){
             if(requestCode == 2) {
-                Uri selectedImage = data.getData();
-                String[] filePathColumn = {MediaStore.Images.Media.DATA};
-                Cursor cursor = getContentResolver().query(selectedImage,
-                        filePathColumn, null, null, null);
-                cursor.moveToFirst();
-                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                String path = cursor.getString(columnIndex);
-                cursor.close();
 
                 Bitmap bitmap = null;
-                try {
-                    bitmap = CarregaFoto.carregaImage(path);
-                    pathImage.setText(path);
-                    if(bitmap == null){
-                        bitmap = CarregaFoto.openPhotoRotate(path, 0);
+                String path = "";
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = getContentResolver().query(selectedImage,
+                            filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    path = cursor.getString(columnIndex);
+                    cursor.close();
+
+                    try {
+                        bitmap = LoadPhoto.loadImage(this, selectedImage, path);
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
+
+                } else {
+                    path = getAbsolutPath(data.getData());
+                    try {
+                        bitmap = LoadPhoto.loadImage(this, data.getData(), path);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
 
+                pathImage.setText(path);
                 imageView.setImageBitmap(bitmap);
 
+            } else if(requestCode == 1){
+                setCapturedImage(getImagePath());
             }
         }
+    }
+
+    public String getImagePath() {
+        return path;
+    }
+
+    private void setCapturedImage(final String imagePath){
+        imageView.setImageBitmap(decodeFile(getRightAngleImage(imagePath)));
+    }
+
+    private String getRightAngleImage(String photoPath) {
+
+        try {
+            ExifInterface ei = new ExifInterface(photoPath);
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            int degree = 0;
+
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_NORMAL:
+                    degree = 0;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degree = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degree = 270;
+                    break;
+                case ExifInterface.ORIENTATION_UNDEFINED:
+                    degree = 0;
+                    break;
+                default:
+                    degree = 90;
+            }
+
+            return rotateImage(degree,photoPath);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return photoPath;
+    }
+
+    private String rotateImage(int degree, String imagePath){
+
+        if(degree<=0){
+            return imagePath;
+        }
+        try{
+            Bitmap b= BitmapFactory.decodeFile(imagePath);
+
+            Matrix matrix = new Matrix();
+            if(b.getWidth()>b.getHeight()){
+                matrix.setRotate(degree);
+                b = Bitmap.createBitmap(b, 0, 0, b.getWidth(), b.getHeight(),
+                        matrix, true);
+            }
+
+            FileOutputStream fOut = new FileOutputStream(imagePath);
+            String imageName = imagePath.substring(imagePath.lastIndexOf("/") + 1);
+            String imageType = imageName.substring(imageName.lastIndexOf(".") + 1);
+
+            FileOutputStream out = new FileOutputStream(imagePath);
+            if (imageType.equalsIgnoreCase("png")) {
+                b.compress(Bitmap.CompressFormat.PNG, 100, out);
+            }else if (imageType.equalsIgnoreCase("jpeg")|| imageType.equalsIgnoreCase("jpg")) {
+                b.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            }
+            fOut.flush();
+            fOut.close();
+
+            b.recycle();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return imagePath;
+    }
+
+    public Bitmap decodeFile(String path) {
+        try {
+            // Decode deal_image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(path, o);
+            // The new size we want to scale to
+            final int REQUIRED_SIZE = 1024;
+
+            // Find the correct scale value. It should be the power of 2.
+            int scale = 1;
+            while (o.outWidth / scale / 2 >= REQUIRED_SIZE && o.outHeight / scale / 2 >= REQUIRED_SIZE)
+                scale *= 2;
+            // Decode with inSampleSize
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = scale;
+            return BitmapFactory.decodeFile(path, o2);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getAbsolutPath(Uri uri){
+        String id = "";
+        if (uri.getLastPathSegment().split(":").length > 1){
+            id = uri.getLastPathSegment().split(":")[1];
+        } else if (uri.getLastPathSegment().split(":").length > 0){
+            id = uri.getLastPathSegment().split(":")[0];
+        }
+        if (id.length() > 0){
+            final String[] imageColumns = {MediaStore.Images.Media.DATA};
+            final Uri tempUri = getUri();
+            Cursor cursor = getContentResolver().query(tempUri,
+                    imageColumns,
+                    MediaStore.Images.Media._ID + "=" + id,
+                    null, null);
+            if (cursor.moveToFirst()) {
+                return cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    private Uri getUri() {
+        String state = Environment.getExternalStorageState();
+        if(!state.equalsIgnoreCase(Environment.MEDIA_MOUNTED))
+            return MediaStore.Images.Media.INTERNAL_CONTENT_URI;
+
+        return MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
     }
 
 
